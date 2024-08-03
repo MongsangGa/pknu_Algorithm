092601de74da3df72c62d67ef15097478615edf6
khjun226 <khjun226@naver.com>
2024-08-03 23:18:57 +0900

    BOJ 11050 / ? / 미해결

---
+++ 1주차/강현준/1주차 BOJ 11050.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fastio cin.tie(0), ios_base::sync_with_stdio(0)
+
+int n, k, g;
+
+
+int main() {
+	fastio;
+
+	cin >> n >> k;
+
+	for(int j = n; j >= ; j--)
+		j *= ;
+
+
+	for (int i = k; i > 0; i--)
+		i *= ;
+
+	g = j / i;
+
+	cout << g;
+}// 음... 어떻게 해야할지 몰라서 팩토리얼 비슷하게 풀어보려고 했는데 안되네요..
