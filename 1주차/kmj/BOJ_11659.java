b2d09005b8e3850d5f0bca96cf9db0e34fb1c85a
김명준 <86913355+mjj111@users.noreply.github.com>
2024-08-03 13:28:52 +0900

    BOJ 11659 / 실버 3/ 15분

--- 1주차/kmj/BOJ_11659.java
+++ 1주차/kmj/BOJ_11659.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class 구간합_구하기4 {
+public class BOJ_11659 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
